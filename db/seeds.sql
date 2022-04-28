dd338c4e74b8c0000766cc09066672a82785047c
bekkahhuss <bekkahhuss@gmail.com>
2022-04-27 21:11:00 -0400

    added route files and fixed schema errors

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,19 +8,19 @@ VALUES
 INSERT INTO role
     (id, salary, title, department_id)
 VALUES
-('11', '25k', 'sales assistant', '1'),
-('12', '40k', 'sales manager', '1'),
-('13', '25k', 'sales assistant', '2'),
-('14', '40k', 'sales manager', '2'),
-('15', '25k', 'sales assistant', '3'),
-('16', '40k', 'sales manager', '3');
+('11', '25000', 'sales assistant', '1'),
+('12', '40000', 'sales manager', '1'),
+('13', '25000', 'sales assistant', '2'),
+('14', '40000', 'sales manager', '2'),
+('15', '25000', 'sales assistant', '3'),
+('16', '40000', 'sales manager', '3');
 
 INSERT INTO employee
-    (1d, first_name, last_name, role_id, manager_id)
+    (id, first_name, last_name, role_id, manager_id)
 VALUES
 ('111', 'Sam' , 'Smith', '11', '222'),
-('222', 'Bob', 'Johnson', '12', ''),
+('222', 'Bob', 'Johnson', '12', '0'),
 ('333', 'Dante', 'Jones', '13', '444'),
-('444', 'Shandra', 'Matthews', '14', ''),
+('444', 'Shandra', 'Matthews', '14', '0'),
 ('555', 'Maya', 'Anderson', '15', '666'),
-('666', 'Steve', 'Michaels', '16', '');
+('666', 'Steve', 'Michaels', '16', '0');
